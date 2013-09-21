3611f17e9455801da2e995466b11e8645fdf99b3
Joost de Valk <joost@yoast.com>
2013-09-21 21:38:20 +0200

    Add product images to the content to make sure the content analysis test catches them,  fixes #5. Add a length check for the product short description, fixes #6.

--- wpseo-woocommerce.php
+++ wpseo-woocommerce.php
@@ -110,6 +110,8 @@ class Yoast_WooCommerce_SEO {
 					add_action( 'admin_footer', array( $this, 'footer_js' ) );
 
 				add_filter( 'wpseo_body_length_score', array( $this, 'change_body_length_requirements' ), 10, 2 );
+				add_filter( 'wpseo_linkdex_results', array( $this, 'add_woocommerce_specific_content_analysis_tests' ), 10, 3 );
+				add_filter( 'wpseo_pre_analysis_post_content', array( $this, 'add_product_images_to_content' ), 10, 2 );
 			}
 		}
 		else if ( $this->license_active ) {
@@ -149,8 +151,8 @@ class Yoast_WooCommerce_SEO {
 	/**
 	 * Changes the body copy length requirements for products.
 	 *
-	 * @param array $lengthReqs
-	 * @param array $job
+	 * @param array $lengthReqs The length requirements currently set.
+	 * @param array $job        The analysis job array.
 	 *
 	 * @return array
 	 */
@@ -164,6 +166,73 @@ class Yoast_WooCommerce_SEO {
 			);
 		}
 		return $lengthReqs;
+	}
+
+	/**
+	 * Check whether the current post is a product, if so, do some WooCommerce specific testing.
+	 *
+	 * @param array  $results The results for the content analysis.
+	 * @param array  $job     The analysis job array.
+	 * @param object $post    The post object for which we're running the analysis.
+	 *
+	 * @return mixed
+	 */
+	function add_woocommerce_specific_content_analysis_tests( $results, $job, $post ) {
+		if ( $post->post_type == 'product' ) {
+			$results = $this->test_short_description( $results, $post );
+		}
+		return $results;
+	}
+
+	/**
+	 * Test whether the short description is actually of decent length.
+	 *
+	 * @param array  $results The results for the content analysis.
+	 * @param object $post    The post object for which we're running the analysis.
+	 *
+	 * @return array
+	 */
+	function test_short_description( $results, $post ) {
+		global $wpseo_metabox, $statistics;
+
+		$word_count = $statistics->word_count( strip_tags( $post->post_excerpt ) );
+		if ( $word_count == 0 ) {
+			$wpseo_metabox->save_score_result( $results, 1, __( 'You should write a short description for this product.', 'yoast-woo-seo' ), 'woocommerce_shortdesc' );
+		}
+		else if ( $word_count < 20 ) {
+			$wpseo_metabox->save_score_result( $results, 5, __( 'The short description for this product too short.', 'yoast-woo-seo' ), 'woocommerce_shortdesc' );
+		}
+		else {
+			$wpseo_metabox->save_score_result( $results, 9, __( 'Your short description has a good length.', 'yoast-woo-seo' ), 'woocommerce_shortdesc' );
+		}
+		return $results;
+	}
+
+	/**
+	 * Make sure the product images are used in calculating the score.
+	 *
+	 * @param string $content The content to modify
+	 * @param object $post    The post object for which we're doing the analysis.
+	 *
+	 * @return string
+	 */
+	function add_product_images_to_content( $content, $post ) {
+		if ( $post->post_type == 'product' ) {
+			$args = array(
+				'post_type'   => 'attachment',
+				'numberposts' => - 1,
+				'post_status' => 'inherit',
+				'post_parent' => $post->ID
+			);
+
+			$attachments = get_posts( $args );
+			if ( $attachments ) {
+				foreach ( $attachments as $attachment ) {
+					$content .= wp_get_attachment_image( $attachment->ID, 'thumbnail' ) . ' ';
+				}
+			}
+		}
+		return $content;
 	}
 
 	/**
